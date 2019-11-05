ed9c300abdccd60e14127fd7a82874c9b6f1231b
Germán Téllez <gertellezv@gmail.com>
2019-11-05 15:33:40 -0500

    Post model added to admin dashboard

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -18,6 +18,8 @@ Including another URLconf
 #Django
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings # Para mostrar imagenes de usuario al darle clic
+from django.conf.urls.static import static # Para mostrar imagenes de usuario al darle clic
 
 from platzigram import views as local_views
 from posts import views as posts_views
@@ -27,5 +29,5 @@ urlpatterns = [
     path('hello-world',local_views.hello_world),
     path('sorted/',local_views.sort_integers),
     path('hi/<str:name>/<int:age>',local_views.say_hi),
-    path('posts/',posts_views.list_posts)
-]
+    path('posts/',posts_views.list_posts),
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,40 @@
+#Django
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Models
+from django.contrib.auth.models import User
+from posts.models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk','title','photo','modified','created')
+    # Opciones por implementar
+    """ list_display_links = ('pk','user')
+    list_editable = ('phone_number','website','picture')
+    search_fields = (
+        'user__username',
+        'user__email',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+        )
+    list_filter = (
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff')
+    fieldsets = (
+        ('Profile',{
+            'fields':(('user','picture'),)
+        }),
+        ('Extra Info',{
+            'fields':(('website','phone_number'),('biography'))
+        }),
+        ('Metadata',{
+            'fields': (('created','modified'),),
+        })
+    )
+    readonly_fields = ('created','modified') """
