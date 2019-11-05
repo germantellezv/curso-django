cd34596206974934c126ab659e76e291f1360054
Germán Téllez <gertellezv@gmail.com>
2019-11-05 13:37:56 -0500

    Create model Post

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 #Django
 from django.contrib import admin
 from django.urls import path
+
 from platzigram import views as local_views
 from posts import views as posts_views
 
