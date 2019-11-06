faae13f3392e1c2e973b62894241883d31dc4ed1
Germán Téllez <gertellezv@gmail.com>
2019-11-06 18:23:49 -0500

    added Sign up and username validation

--- users/views.py
+++ users/views.py
@@ -2,7 +2,11 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from users.models import Profile
 
+# Exceptions
+from django.db.utils import IntegrityError
 
 # Create your views here.
 
@@ -22,3 +26,28 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html',{'error':'Password confirmation does not match'})
+        # Crear el usuario
+        try:    
+            user = User.objects.create_user(username = username, password = passwd)
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name']
+            user.email = request.POST['email']
+            user.save()
+        except IntegrityError:
+            return render(request, 'users/signup.html',{'error':'This username is already in use'})
+
+        # Crear el perfil al usuario
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
